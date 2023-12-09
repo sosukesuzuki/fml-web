7c0d0140142d7373fce2556f64cdc176012562c9
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 18:33:58 +0900

    Use fml from browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { default: module } = await import("lib-fml-web");
+      const Module = await module();
+      const api = {
+        parse: Module.cwrap("parse", "string", ["string"], []),
+      };
+      const result = api.parse("a|a");
+      console.log(result);
+    })();
+  }, []);
+
   return <h1>Hello</h1>;
 }
