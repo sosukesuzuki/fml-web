d837f7b0901de5af243db4117dfc84b44fbb1870
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 17:16:19 +0900

    Remove submodule

--- lib/fml/fml_compiler.h
+++
@@ -1,9 +0,0 @@
-#ifndef FML_COMPILER_H
-#define FML_COMPILER_H
-
-#include "fml_instructions.h"
-#include "fml_parser.h"
-
-int compile(Node* node, Instructions* instructions);
-
-#endif
--- lib/fml/fml_lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef FML_LIB_H
-#define FML_LIB_H
-
-int fmlTest(char* regexp, char* str);
-
-#endif
