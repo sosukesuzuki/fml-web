f1a3e24fb351ad8283e0fb8aecab3abc4a6254a0
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 19:16:52 +0900

    Add `compileToInstructions` function

--- lib/fml_web.c
+++ lib/fml_web.c
@@ -1,5 +1,10 @@
+#include "fml/fml_compiler.h"
+#include "fml/fml_instructions.h"
 #include "fml/fml_parser.h"
+
+#include "fml_instructions_to_json.h"
 #include "fml_node_to_json.h"
+
 #include <emscripten.h>
 #include <stdlib.h>
 
@@ -9,6 +14,21 @@ char* parse(char* input)
     Node* node = malloc(sizeof(Node));
     int result = parseRegexp(node, input);
     char* output = nodeToJson(node);
+    return output;
+}
+
+EMSCRIPTEN_KEEPALIVE
+char* compileToInstructions(char* input)
+{
+    Node* node = malloc(sizeof(Node));
+    parseRegexp(node, input);
+
+    Instructions* instructions = malloc(sizeof(Instructions));
+    initInstructions(instructions);
+    compile(node, instructions);
+
+    char* output = instructionsToJson(instructions);
+
     return output;
 }
 
