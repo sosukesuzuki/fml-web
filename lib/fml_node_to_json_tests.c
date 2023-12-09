278bb9fc405949129d450ec277e9673358a71a02
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 17:32:21 +0900

    Add basic tests

--- lib/fml_node_to_json_tests.c
+++ lib/fml_node_to_json_tests.c
@@ -1,3 +1,4 @@
+#include "fml/fml_parser.h"
 #include "fml_node_to_json.h"
 #include "fml_test_utils.h"
 
@@ -9,8 +10,52 @@ void nodeToJson_01()
     free(json);
 }
 
+void nodeToJson_02()
+{
+    Node node;
+    char* input = "a";
+    parseRegexp(&node, input);
+    char* json = nodeToJson(&node);
+    TEST_ASSERT(strcmp(json, "{\"char\":\"a\"}") == 0, 1);
+    free(json);
+}
+
+void nodeToJson_03()
+{
+    Node node;
+    char* input = "a|b";
+    parseRegexp(&node, input);
+    char* json = nodeToJson(&node);
+    TEST_ASSERT(strcmp(json, "{\"alt\":[{\"char\":\"a\"},{\"char\":\"b\"}]}") == 0, 1);
+    free(json);
+}
+
+void nodeToJson_04()
+{
+    Node node;
+    char* input = "ab";
+    parseRegexp(&node, input);
+    char* json = nodeToJson(&node);
+    TEST_ASSERT(strcmp(json, "{\"concat\":[{\"char\":\"a\"},{\"char\":\"b\"}]}") == 0, 1);
+    free(json);
+}
+
+void nodeToJson_05()
+{
+    Node node;
+    char* input = "a*";
+    parseRegexp(&node, input);
+    char* json = nodeToJson(&node);
+    TEST_ASSERT(strcmp(json, "{\"qtfr\":{\"char\":\"a\"}}") == 0, 1);
+    free(json);
+}
+
 int main()
 {
     RUN_TEST(nodeToJson_01);
+    RUN_TEST(nodeToJson_02);
+    RUN_TEST(nodeToJson_03);
+    RUN_TEST(nodeToJson_04);
+    RUN_TEST(nodeToJson_05);
     return 0;
 }
