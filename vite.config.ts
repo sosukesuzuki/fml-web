72a62e9cb4762d5fcd237bce3e863da9d8cbffbf
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 18:18:48 +0900

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/fml-web/",
-  plugins: [react()],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./lib/fml_web.wasm",
+          dest: ".",
+        },
+      ],
+    }),
+    react(),
+  ],
 });
