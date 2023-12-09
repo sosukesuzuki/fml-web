fe46ae38e0af5b13e54428bcb3a39fc3ea228d3a
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 18:45:37 +0900

    Add `@ts-ignore` comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     (async () => {
+      // @ts-ignore
       const { default: module } = await import("lib-fml-web");
       const Module = await module();
       const api = {
