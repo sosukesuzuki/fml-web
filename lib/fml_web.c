2f277b6e455b5f8f199bcaad8b148ba2905adfec
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-12-09 17:50:05 +0900

    Add build target `fml_web`

--- lib/fml_web.c
+++ lib/fml_web.c
@@ -1,6 +1,19 @@
 #include "fml/fml_parser.h"
+#include "fml_node_to_json.h"
 #include <emscripten.h>
 #include <stdlib.h>
 
 EMSCRIPTEN_KEEPALIVE
-int parse(char* input) { return 1 }
+char* parse(char* input)
+{
+    Node* node = malloc(sizeof(Node));
+    int result = parseRegexp(node, input);
+    char* output = nodeToJson(node);
+    return output;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void freeString(char* str)
+{
+    free(str);
+}
